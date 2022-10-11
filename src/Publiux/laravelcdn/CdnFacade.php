dbb7c8a9eba6409976fb47c8f2923e32d099d7cd
Khoubeib <khoubeib@zembratech.com>
2022-10-11 14:38:04 -0700

    Allow user to force/skip bypass

--- src/Publiux/laravelcdn/CdnFacade.php
+++ src/Publiux/laravelcdn/CdnFacade.php
@@ -3,196 +3,183 @@
 namespace Publiux\laravelcdn;
 
 use Illuminate\Support\Facades\Request;
+use InvalidArgumentException;
 use Publiux\laravelcdn\Contracts\CdnFacadeInterface;
 use Publiux\laravelcdn\Contracts\CdnHelperInterface;
 use Publiux\laravelcdn\Contracts\ProviderFactoryInterface;
 use Publiux\laravelcdn\Exceptions\EmptyPathException;
+use Publiux\laravelcdn\Providers\Contracts\ProviderInterface;
 use Publiux\laravelcdn\Validators\CdnFacadeValidator;
 
 /**
  * Class CdnFacade.
  *
- * @category
- *
  * @author  Mahmoud Zalt <mahmoud@vinelab.com>
  */
-class CdnFacade implements CdnFacadeInterface
-{
-    /**
-     * @var array
-     */
-    protected $configurations;
+class CdnFacade implements CdnFacadeInterface {
 
-    /**
-     * @var \Publiux\laravelcdn\Contracts\ProviderFactoryInterface
-     */
-    protected $provider_factory;
+	/** @var array */
+	protected $configurations;
 
-    /**
-     * instance of the default provider object.
-     *
-     * @var \Publiux\laravelcdn\Providers\Contracts\ProviderInterface
-     */
-    protected $provider;
+	/** @var ProviderFactoryInterface */
+	protected $providerFactory;
 
-    /**
-     * @var \Publiux\laravelcdn\Contracts\CdnHelperInterface
-     */
-    protected $helper;
+	/**
+	 * instance of the default provider object.
+	 *
+	 * @var ProviderInterface
+	 */
+	protected $provider;
 
-    /**
-     * @var \Publiux\laravelcdn\Validators\CdnFacadeValidator
-     */
-    protected $cdn_facade_validator;
+	/** @var CdnHelperInterface */
+	protected $helper;
 
-    /**
-     * Calls the provider initializer.
-     *
-     * @param \Publiux\laravelcdn\Contracts\ProviderFactoryInterface $provider_factory
-     * @param \Publiux\laravelcdn\Contracts\CdnHelperInterface       $helper
-     * @param \Publiux\laravelcdn\Validators\CdnFacadeValidator      $cdn_facade_validator
-     */
-    public function __construct(
-        ProviderFactoryInterface $provider_factory,
-        CdnHelperInterface $helper,
-        CdnFacadeValidator $cdn_facade_validator
-    ) {
-        $this->provider_factory = $provider_factory;
-        $this->helper = $helper;
-        $this->cdn_facade_validator = $cdn_facade_validator;
+	/** @var CdnFacadeValidator */
+	protected $cdnFacadeValidator;
 
-        $this->init();
-    }
+	/**
+	 * Calls the provider initializer.
+	 *
+	 * @param  ProviderFactoryInterface  $providerFactory
+	 * @param  CdnHelperInterface        $helper
+	 * @param  CdnFacadeValidator        $cdFacadeValidator
+	 */
+	public function __construct(ProviderFactoryInterface $providerFactory, CdnHelperInterface $helper, CdnFacadeValidator $cdFacadeValidator) {
+		$this->providerFactory = $providerFactory;
+		$this->helper = $helper;
+		$this->cdnFacadeValidator = $cdFacadeValidator;
 
-    /**
-     * Read the configuration file and pass it to the provider factory
-     * to return an object of the default provider specified in the
-     * config file.
-     */
-    private function init()
-    {
-        // return the configurations from the config file
-        $this->configurations = $this->helper->getConfigurations();
+		$this->init();
+	}
 
-        // return an instance of the corresponding Provider concrete according to the configuration
-        $this->provider = $this->provider_factory->create($this->configurations);
-    }
+	/**
+	 * Read the configuration file and pass it to the provider factory
+	 * to return an object of the default provider specified in the
+	 * config file.
+	 */
+	private function init() {
+		// return the configurations from the config file
+		$this->configurations = $this->helper->getConfigurations();
 
-    /**
-     * this function will be called from the 'views' using the
-     * 'Cdn' facade {{Cdn::asset('')}} to convert the path into
-     * it's CDN url.
-     *
-     * @param $path
-     *
-     * @throws Exceptions\EmptyPathException
-     *
-     * @return mixed
-     */
-    public function asset($path)
-    {
-        // if asset always append the public/ dir to the path (since the user should not add public/ to asset)
-        return $this->generateUrl($path, 'public/');
-    }
+		// return an instance of the corresponding Provider concrete according to the configuration
+		$this->provider = $this->providerFactory->create($this->configurations);
+	}
 
-    /**
-     * check if package is surpassed or not then
-     * prepare the path before generating the url.
-     *
-     * @param        $path
-     * @param string $prepend
-     *
-     * @return mixed
-     */
-    private function generateUrl($path, $prepend = '')
-    {
-        // if the package is surpassed, then return the same $path
-        // to load the asset from the localhost
-        if (isset($this->configurations['bypass']) && $this->configurations['bypass']) {
-            return Request::root().'/'.$path;
-        }
+	/**
+	 * this function will be called from the 'views' using the
+	 * 'Cdn' facade {{Cdn::asset('')}} to convert the path into
+	 * it's CDN url.
+	 *
+	 * @param             $path
+	 * @param  bool|null  $overrideBypass
+	 *
+	 * @return mixed
+	 * @throws Exceptions\EmptyPathException
+	 *
+	 */
+	public function asset($path, ?bool $overrideBypass = null) {
+		// if asset always append the public/ dir to the path (since the user should not add public/ to asset)
+		return $this->generateUrl($path, 'public/', $overrideBypass);
+	}
 
-        if (!isset($path)) {
-            throw new EmptyPathException('Path does not exist.');
-        }
+	/**
+	 * check if package is surpassed or not then
+	 * prepare the path before generating the url.
+	 *
+	 * @param             $path
+	 * @param  string     $prepend
+	 * @param  bool|null  $overrideBypass
+	 *
+	 * @return mixed
+	 */
+	private function generateUrl($path, $prepend = '', ?bool $overrideBypass = null) {
+		// if the package is surpassed, then return the same $path
+		// to load the asset from the localhost
+		$bypass = $overrideBypass ?? (isset($this->configurations['bypass']) && $this->configurations['bypass']);
+		if ($bypass) {
+			return Request::root() . '/' . $path;
+		}
 
-        // Add version number
-        //$path = str_replace(
-        //    "build",
-        //    $this->configurations['providers']['aws']['s3']['version'],
-        //    $path
-        //);
+		if (!isset($path)) {
+			throw new EmptyPathException('Path does not exist.');
+		}
 
-        // remove slashes from begging and ending of the path
-        // and append directories if needed
-        $clean_path = $prepend.$this->helper->cleanPath($path);
+		// Add version number
+		//$path = str_replace(
+		//    "build",
+		//    $this->configurations['providers']['aws']['s3']['version'],
+		//    $path
+		//);
 
-        // call the provider specific url generator
-        return $this->provider->urlGenerator($clean_path);
-    }
+		// remove slashes from begging and ending of the path
+		// and append directories if needed
+		$clean_path = $prepend . $this->helper->cleanPath($path);
 
-    /**
-     * this function will be called from the 'views' using the
-     * 'Cdn' facade {{Cdn::mix('')}} to convert the Laravel 5.4 webpack mix
-     * generated file path into it's CDN url.
-     *
-     * @param $path
-     *
-     * @return mixed
-     *
-     * @throws Exceptions\EmptyPathException, \InvalidArgumentException
-     */
-    public function mix($path)
-    {
-        static $manifest = null;
-        if (is_null($manifest)) {
-            $manifest = json_decode(file_get_contents(public_path('mix-manifest.json')), true);
-        }
-        if (isset($manifest['/' . $path])) {
-            return $this->generateUrl($manifest['/' . $path], 'public/');
-        }
-        if (isset($manifest[$path])) {
-            return $this->generateUrl($manifest[$path], 'public/');
-        }
-        throw new \InvalidArgumentException("File {$path} not defined in asset manifest.");
-    }
+		// call the provider specific url generator
+		return $this->provider->urlGenerator($clean_path);
+	}
 
-    /**
-     * this function will be called from the 'views' using the
-     * 'Cdn' facade {{Cdn::elixir('')}} to convert the elixir generated file path into
-     * it's CDN url.
-     *
-     * @param $path
-     *
-     * @throws Exceptions\EmptyPathException, \InvalidArgumentException
-     *
-     * @return mixed
-     */
-    public function elixir($path)
-    {
-        static $manifest = null;
-        if (is_null($manifest)) {
-            $manifest = json_decode(file_get_contents(public_path('build/rev-manifest.json')), true);
-        }
-        if (isset($manifest[$path])) {
-            return $this->generateUrl('build/' . $manifest[$path], 'public/');
-        }
-        throw new \InvalidArgumentException("File {$path} not defined in asset manifest.");
-    }
+	/**
+	 * this function will be called from the 'views' using the
+	 * 'Cdn' facade {{Cdn::mix('')}} to convert the Laravel 5.4 webpack mix
+	 * generated file path into it's CDN url.
+	 *
+	 * @param $path
+	 *
+	 * @return mixed
+	 * @throws Exceptions\EmptyPathException
+	 * @throws InvalidArgumentException
+	 */
+	public function mix($path) {
+		static $manifest = null;
+		if (is_null($manifest)) {
+			$manifest = json_decode(file_get_contents(public_path('mix-manifest.json')), true);
+		}
 
-    /**
-     * this function will be called from the 'views' using the
-     * 'Cdn' facade {{Cdn::path('')}} to convert the path into
-     * it's CDN url.
-     *
-     * @param $path
-     *
-     * @throws Exceptions\EmptyPathException
-     *
-     * @return mixed
-     */
-    public function path($path)
-    {
-        return $this->generateUrl($path);
-    }
+		if (isset($manifest['/' . $path])) {
+			return $this->generateUrl($manifest['/' . $path], 'public/');
+		}
+
+		if (isset($manifest[$path])) {
+			return $this->generateUrl($manifest[$path], 'public/');
+		}
+
+		throw new InvalidArgumentException("File {$path} not defined in asset manifest.");
+	}
+
+	/**
+	 * this function will be called from the 'views' using the
+	 * 'Cdn' facade {{Cdn::elixir('')}} to convert the elixir generated file path into
+	 * it's CDN url.
+	 *
+	 * @param $path
+	 *
+	 * @return mixed
+	 * @throws Exceptions\EmptyPathException
+	 * @throws InvalidArgumentException
+	 */
+	public function elixir($path) {
+		static $manifest = null;
+		if (is_null($manifest)) {
+			$manifest = json_decode(file_get_contents(public_path('build/rev-manifest.json')), true);
+		}
+		if (isset($manifest[$path])) {
+			return $this->generateUrl('build/' . $manifest[$path], 'public/');
+		}
+		throw new InvalidArgumentException("File {$path} not defined in asset manifest.");
+	}
+
+	/**
+	 * this function will be called from the 'views' using the
+	 * 'Cdn' facade {{Cdn::path('')}} to convert the path into
+	 * it's CDN url.
+	 *
+	 * @param $path
+	 *
+	 * @return mixed
+	 * @throws Exceptions\EmptyPathException
+	 */
+	public function path($path) {
+		return $this->generateUrl($path);
+	}
 }
